8b9f79f7fd1a9f3e3a52c7f2e4061b11de745f85
tjasa <tjasa.zilavec@gmail.com>
2023-12-12 10:44:29 +0100

    .made simple node.js server

---
+++ data/images.js
@@ -0,0 +1,53 @@
+const imageCache = new Map([
+  [5, {
+  "title": "Rose",
+  "overlayColor": "#ff0072",
+  "imageSrc": "https://www.gstatic.com/webp/gallery3/1.sm.png"
+  }],
+  [3, {
+  "title": "Tree",
+  "overlayColor": "#00c6ff",
+  "imageSrc": "https://www.gstatic.com/webp/gallery/4.sm.webp"
+  }],
+  [7, {
+  "title": "Fire",
+  "overlayColor": "#ffc600",
+  "imageSrc": "https://www.gstatic.com/webp/gallery/5.webp"
+  }],
+  [4, {
+  "title": "Penguin",
+  "overlayColor": "#ffc600",
+  "imageSrc": "https://www.gstatic.com/webp/gallery3/2_webp_a.png"
+  }],
+  [9, {
+  "title": "Dice",
+  "overlayColor": "#ffc600",
+  "imageSrc": "https://www.gstatic.com/webp/gallery3/3_webp_a.sm.png"
+  }],
+  [1, {
+  "title": "Compass",
+  "overlayColor": "#00c6ff",
+  "imageSrc": "https://www.gstatic.com/webp/gallery3/5_webp_a.sm.png"
+  }],
+  [10, {
+  "title": "River",
+  "overlayColor": "#65ff00",
+  "imageSrc": "https://www.gstatic.com/webp/gallery/3.sm.webp"
+  }],
+  [15, {
+  "title": "Kayak",
+  "overlayColor": "#00c6ff",
+  "imageSrc": "https://www.gstatic.com/webp/gallery/2.webp"
+  }],
+  [2, {
+  "title": "Valley",
+  "overlayColor": "#65ff00",
+  "imageSrc": "https://www.gstatic.com/webp/gallery/1.sm.webp"
+  }],
+  [8, {
+  "title": "Rose",
+  "overlayColor": "#ff0072",
+  "imageSrc": "https://www.gstatic.com/webp/gallery3/1.sm.png"
+  }],
+  ]);
+  module.exports = imageCache;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 const express =require('express');
 const app=express();
 const port=3000;
+const imageCache=require('../data/images.js');
 
-app.get('/', (req, res)=>{
-  res.send('This server is working :D');
-});
+/** this is a function that will help simulate delay  -> this is because the endpoint should sleep for 1-4 seconds before returning the result*/
+let sleep=(ms)=>{
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+/** First endpoint => provides a list of image identifiers; is hardcoded*/
+const getImageIds=()=>{
+  return [5, 3, 7, 4, 9, 1, 10, 15, 2, 8];
+  }
+
+  /** on this endpoint we get array of ids that are in the getImageIds function */
+  app.get('/api/images', (req, res)=>{
+    res.json(getImageIds);
+  })
+
+  /** second endpoint => provides the details of an image; GET request with the image ID as a path parameter */
+app.get('/api/image/:imageId', async(req,res)=>{
+const imageId=parseInt(req.params.imageId);
+const processTime=Math.floor((Math.random()*3)+1); /** why 3 + 1 and not *4? Because Math.random can be 0, which means the server will return the results imediately and we want a 1 - 4 second delay. That is why we add +1 */
+await sleep(processTime * 1000); /** add random number to the delay function */
+const imageDetails=imageCache.get(imageId);
+res.json(imageDetails);
+})
+
 app.listen(port, ()=>{
-  console.log(`server is running on port ${port}`);
+  console.log(`server is running on port http://localhost:${port}`);
 })
